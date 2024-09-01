43e224a1d427cd2ffeeec322984cdff588b30ed6
HimanshuBhandariGtHub <himanshubhandari2023.com>
2024-09-01 17:58:27 +0530

    default template

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading=<h1>Food Paradise App</h1>;
+
+const Heading =()=>{
+    return (
+        <div className="firstDiv">
+            {heading}
+        </div>
+    )
+}
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>);
