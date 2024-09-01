c0fb26ccc36ab2be202e7a55a487e3af2bef243f
HimanshuBhandariGtHub <himanshubhandari2023.com>
2024-09-01 18:25:37 +0530

    day 1-build nav n setup

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=<h1>Food Paradise App</h1>;
+//PLANNING- what components my app ll have for our Ref
+// Header
+//     Logo
+//     Nav items
+// Body    
+//     Search
+//     RestaurantContainer
+//         RestaurantCard
+// Footer
+//     Copyright
+//     Links
+//     Address
+//     Contact
 
-const Heading =()=>{
-    return (
-        <div className="firstDiv">
-            {heading}
+const Header =()=>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://banner2.cleanpng.com/20180720/spp/956a15ac3b67af3b8619d9a8771d1399.webp"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
-    )
-}
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>);
+    );
+};
+
+const AppLayout =()=>{
+    return(
+        <div className="app">
+            <Header />
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
