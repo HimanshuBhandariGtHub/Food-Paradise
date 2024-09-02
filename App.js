f5a34563128ad8bb4d7450739e95acba9a8a7909
HimanshuBhandariGtHub <himanshubhandari2023.com>
2024-09-02 07:53:59 +0530

    added res card in body

--- App.js
+++ App.js
@@ -33,10 +33,50 @@ const Header =()=>{
     );
 };
 
+
+const RestaurantCard =({resName, cuisine})=>{
+   
+    return(
+        <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+            <img className="res-logo" alt="res-logo" src="https:/banner2.cleanpng.com/20180720/spp/956a15ac3b67af3b8619d9a8771d1399.webp" />
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 mins</h4>
+        </div>
+    );
+};
+
+const Body =()=>{
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+
+                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+
+            </div>
+        </div>
+    );
+};
+
 const AppLayout =()=>{
     return(
         <div className="app">
             <Header />
+            <Body />
         </div>
     );
 };
